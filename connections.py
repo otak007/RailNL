c00923fe7ff828518a5a42d8fc6e4bb8e2eeb448
Pjotjee <pjotrvanhulst@hotmail.com>
2019-05-07 13:50:32 +0200

    DumbPaths

--- connections.py
+++ connections.py
@@ -9,7 +9,7 @@ Created on Mon Apr  8 21:07:01 2019
 
 # Initial connection objects
 class Connection(object):
-    def __init__(self, stationA, stationB, travelTime, chooseConnection):
+    def __init__(self, stationA, stationB, travelTime, index, chooseConnection):    #index,
         self.stationA = stationA
         self.stationB = stationB
         self.travelTime = int(travelTime)
@@ -17,3 +17,4 @@ class Connection(object):
         self.travelled = False
         self.critical = False
         self.value = 0
+        self.index = index
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from stations import Station
 from traject import Traject
 import csv
 import matplotlib.pyplot as plt
-        
+
 class Map(object):
 
     def __init__(self):
@@ -14,82 +14,82 @@ class Map(object):
 
     def load_connections(self):
         # Read excel file and create a list with connection objects
-        with open('ConnectiesHolland.csv') as csv_file:
+        with open('ConnectiesHolland.csv', 'r') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-        
+
             connections = []
             for row in csv_reader:
-                connections.append(Connection(row[0], row[1], row[2], False)) 
+                connections.append(Connection(row[0], row[1], row[2], False))
             return connections
 
     def load_stations(self):
         # Read excel file and create a list with station objects
         with open('StationsHolland.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
-        
+
             stations = []
-        
+
             for row in csv_reader:
                 stations.append(Station(row[0], row[1], row[2], row[3]))
                 plt.plot(float(row[2]), float(row[1]), "bo")
                 plt.text(float(row[2]) -0.02, float(row[1])+0.02, row[0], fontsize=5)
             return stations
-    
+
     def is_critical(self):
-        
+
         # Checks if connection is connected to critical station
         for station in self.stations:
             for connection in self.connections:
                 if (station.name == connection.stationA or station.name == connection.stationB) and station.critical == True:
                     connection.critical = True
                     connection.value = 1
-    
-    
+
+
     def plot(self):
-        
+
         for station in self.stations:
-            
+
             for connection in self.connections:
-                
+
                 if (station.name == connection.stationA or station.name == connection.stationB) and connection.chooseConnection == False:
                    connection.chooseConnection = True
-    
+
                    for station2 in self.stations:
                        if station2.name == connection.stationB or station2.name == connection.stationA:
                            if station.critical or station2.critical:
                                plt.plot([float(station.yCoordinate), float(station2.yCoordinate)], [float(station.xCoordinate), float(station2.xCoordinate)], 'y-')
                            else:
                                plt.plot([float(station.yCoordinate), float(station2.yCoordinate)], [float(station.xCoordinate), float(station2.xCoordinate)], 'y--')
-                        
+
     def new_traject(self, name, station, color):
-        
+
         # Set travelled connections to false
         for connection in self.connections:
             connection.travelled == False
-            
+
         # Create new traject
         traject = Traject(name, station)
-        
+
         # Transform string to object
         for row in self.stations:
             if station == row.name:
                 start_station = row
                 break
-        
-        
+
+
         self.travel(traject, start_station, color)
-        
+
     def travel(self, traject, station, color):
-        
+
         # Maximum time is 120 mins
         if traject.total_time < 120:
-            
+
             # Initialize lists
             possible_names = []
             possible_times = []
             possible_values = []
             critical_conns = []
-            
+
             # Add relevant connections to relevant lists
             for connection in self.connections:
                 if connection.travelled == False:
@@ -101,13 +101,13 @@ class Map(object):
                         possible_names.append(connection.stationA)
                         possible_times.append(connection.travelTime)
                         possible_values.append(connection.value)
-            
+
             # Add critical connections to list
             for x in range(len(possible_values)):
                 if possible_values[x] == 1:
                     critical_conns.append(possible_times[x])
-            
-            
+
+
             # Check which station is closest, first check for critical connections
             if critical_conns:
                 closest_distance = min(critical_conns)
@@ -116,28 +116,28 @@ class Map(object):
             else:
                 print(traject.traject)
                 return traject.traject
-                        
-                                    
+
+
             # If maximum time is not yet exceeded, add fastest connection to traject
             if traject.total_time + closest_distance < 120:
                 traject.total_time += closest_distance
-                
+
                 # Transform string to station
                 next_station = possible_names[possible_times.index(closest_distance)]
                 for row in self.stations:
                     if next_station == row.name:
                         next_station = row
                         break
-                
-    
-                # Set travelled to true and plot 
+
+
+                # Set travelled to true and plot
                 for connection in self.connections:
                     if (station.name == connection.stationA or station.name == connection.stationB) and (next_station.name == connection.stationA or next_station.name == connection.stationB):
                         connection.travelled = True
-                
+
                 # Reset current station
                 traject.traject.append(station.name)
-                
+
                 current_station = next_station
                 plt.plot([float(station.yCoordinate), float(next_station.yCoordinate)], [float(station.xCoordinate), float(next_station.xCoordinate)], color+"-")
 
@@ -148,11 +148,11 @@ class Map(object):
                 return traject.traject
         print(traject.total_time)
 
-            
-            
-                        
-                               
-                               
+
+
+
+
+
 if __name__ == "__main__":
     # Initialize map, plot and give four starting stations
     NH = Map()
