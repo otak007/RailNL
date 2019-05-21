3fe4579a71c909513f47465130eb57cc5827d3d2
Pjotjee <pjotrvanhulst@hotmail.com>
2019-05-21 14:23:29 +0200

    rm dijkstra

--- randomTrajectenSA.py
+++ randomTrajectenSA.py
@@ -19,7 +19,8 @@ class Map(object):
 
     def load_connections(self):
         # Read excel file and create a list with connection objects
-        with open('ConnectiesNationaal.csv') as csv_file:
+        #with open('ConnectiesNationaal.csv') as csv_file:
+        with open('ConnectiesHolland.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
 
             connections = []
@@ -29,7 +30,8 @@ class Map(object):
 
     def load_stations(self):
         # Read excel file and create a list with station objects
-        with open('StationsNationaal.csv') as csv_file:
+        #with open('StationsNationaal.csv') as csv_file:
+        with open('StationsHolland.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
 
             stations = []
@@ -57,10 +59,11 @@ class Map(object):
     def choose_trajecten(self):
 
         max_number_trajects = 20
-        tot_num_critical = 89
+        tot_num_critical = 20
         trajecten = self.random_traject()
 
-        for num_trajects in range(1, max_number_trajects+1):
+        #for num_trajects in range(1, max_number_trajects+1):
+        for num_trajects in range(4 , 5):
             start = time.time()
 
 
@@ -70,7 +73,6 @@ class Map(object):
             x = []
             found_kBest = []
             found_k = []
-
 
             # Choose 4 trajects twice and remember the 4 with the highest K, repeat this 1 000 000 times
             #for i in range(1000000):
@@ -125,35 +127,44 @@ class Map(object):
 
             end = time.time()
 
-            print("ok ")
-            strt_station = trajecten[1].traject[0]
-            iteraties_SA = 3
-            #Choose a traject to optimize
-            x = randint(0, num_trajects - 1)
 
-            trajectSA = trajecten[x].traject
-            if (trajecten[x].traject != None):
-                for i in range(iteraties_SA):
-                    temp = (iteraties_SA/(iteraties_SA - i))*100
-                    #print(trajecten)
-                    trajecten = SimulatedAnnealing.SA(self, x, trajecten, temp, num_trajects, self.connections, self.stations)
-                    #print(trajecten)
+        strt_station = trajecten[1].traject[0]
+        iteraties_SA = 100
+
+
+        #for t in range(num_trajects):
+            #print(trajecten[t].traject)
+        print("VOLGENDE TRAJECT", )
+        #trajectSA = trajecten[x].traject
+        #if (trajecten[x].traject != None):
+        for i in range(iteraties_SA):
+            temp = ((iteraties_SA - i)/(iteraties_SA))*100
+            #Choose a traject to change
+            x = randint(0, num_trajects - 1)
+            trajecten = SimulatedAnnealing.SA(self, x, trajecten, temp, num_trajects, self.connections, self.stations)
+            K = SimulatedAnnealing.Calculate_K(trajecten, num_trajects)
+            print("K is: ", K)
+
+        #Plot of the temperature:
+        x = [i for i in range(iteraties_SA)]
+        y = [(((iteraties_SA - i)/(iteraties_SA))*100) for i in x]
+        plt.plot(x,y)
 
         #temp = 1000
         #trajecten = SimulatedAnnealing.SA(self, trajecten, temp, num_trajects, self.connections, self.stations)
 
-            for i in range(num_trajects):
+            #for i in range(num_trajects):
                 #print(trajecten[options[i]].traject)
                 #print("traveltime ", trajecten[options[i]].total_time)
 
-                f.write("\nBest traject:\n")
-                for i in range(num_trajects):
-                    f.write(str(trajecten[options[i]].traject)+ "\n")
-                    f.write(str("traveltime "+ str(trajecten[options[i]].total_time) + "\n"))
+                #f.write("\nBest traject:\n")
+                #for i in range(num_trajects):
+                #    f.write(str(trajecten[options[i]].traject)+ "\n")
+                #    f.write(str("traveltime "+ str(trajecten[options[i]].total_time) + "\n"))
 
-                f.write("Total traveltime: " +str(traveltime)+"\n")
-                f.write("K: "+ str(k) +"\n")
-                f.write("CPT: "+ str(end-start))
+                #f.write("Total traveltime: " +str(traveltime)+"\n")
+                #f.write("K: "+ str(k) +"\n")
+                #f.write("CPT: "+ str(end-start))
 
 
 
@@ -168,6 +179,7 @@ class Map(object):
         trajecten = []
         for x in range(10):
             index = random.randint(0, len(self.stations) - 1)
+            # begin station is een random station uit de lijst
             begin_station = self.stations[index].name
             trajecten.append(self.new_traject(str(x), begin_station, "c"))
         return trajecten
@@ -179,7 +191,6 @@ class Map(object):
             if station == row.name:
                 start_station = row
                 break
-
         return self.travel(traject, start_station, color)
 
 
@@ -191,7 +202,6 @@ class Map(object):
         possible_times = []
         possible_critical = []
 
-        max_min = 180
 
         self.is_critical()
         max_min = 120
@@ -206,17 +216,16 @@ class Map(object):
                     possible_times.append(connection.travelTime)
                     possible_connections.append(connection)
                     possible_critical.append(connection.critical)
-
                 elif station.name == connection.stationB and connection.travelTime + traject.total_time < max_min:
                     possible_names.append(connection.stationA)
                     possible_times.append(connection.travelTime)
                     possible_connections.append(connection)
                     possible_critical.append(connection.critical)
 
+
             # return the traject when no other connection is possible to add to the traject
             if len(possible_connections) == 0:
                 return;
-
             # Not possible choose the same connection 2 times in a row, except the station has only 1 connection
             if len(traject.traject) > 0 and len(possible_names) > 1 and possible_names.count(traject.traject[-1]) > 0:
                 delete_index = possible_names.index(traject.traject[-1])
@@ -229,7 +238,7 @@ class Map(object):
             next_station = possible_names[random_index]
             traject.total_time += possible_times[random_index]
 
-
+            #print("4:", traject)
             for row in self.stations:
                 if next_station == row.name:
                     next_station = row
@@ -238,13 +247,15 @@ class Map(object):
             # Reset current station
             traject.traject.append(station.name)
             traject.connections.append(possible_connections[random_index])
+            #traject.traject.append(possible_connections[random_index])
 
             current_station = next_station
-
 
             # Travel again with
             self.travel(traject, current_station, color)
 
+        #print("In: travel geeft terug: ", )
+        #traject.print_all()
         return traject
 
 
@@ -257,6 +268,6 @@ if __name__ == "__main__":
     print("CPT: ", end-start)
     plt.show()
 
-    input = NH.choose_trajecten().trajecten
+    #input = NH.choose_trajecten().trajecten
 
-    print(isinstance(traject, (tuple, list, dict, set)))
+    #print(isinstance(traject, (tuple, list, dict, set)))
